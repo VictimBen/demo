61686424bc70cc11b276c55928dfa84111bf664b
victim_ben <victim_ben@163.com>
2019-07-09 15:31:32 +0800

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public HelloWorld{
+	public static void main(String[] args){
+System.out.println("1");
+		System.out.println("hello");
+		System.out.println("2");
+		System.out.println("world");
+		
+}
+}
